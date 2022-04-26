7a89f2099193ac395438e7152fbb50065dc54864
hamperedwings41 <92867293+hamperedwings41@users.noreply.github.com>
2022-04-26 16:34:11 -0500

    Add files via upload

--- Screen.java
+++ Screen.java
@@ -21,9 +21,17 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 	private File Turt;
 	private File FrogIMG;
 	
+	private File Truck;
+	private File car;
+	private File carpng;
+	private File tractor;
+	private File racecar;
+	
+	
 	private int MouseX;
 	private int MouseY;
 	private int playerX, playerY;
+	private int score = 0;
 	
 	private int moveDistance = 50;
 	
@@ -51,6 +59,31 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 	private Turtle down;
 	private Turtle down2;
 	private Turtle down3;
+	
+	private Car car1;
+	private Car car2;
+	private Car car3;
+	private Car car4;
+	
+	private Car car5;
+	private Car car6;
+	private Car car7;
+	private Car car8;
+	
+	private Car tractor1;
+	private Car tractor2;
+	private Car tractor3;
+	private Car tractor4;
+	
+	private Car racecar1;
+	private Car racecar2;
+	private Car racecar3;
+	private Car racecar4;
+	
+	private Car truck1;
+	private Car truck2;
+	private Car truck3;
+	private Car truck4;
 
 	
 	public Screen()
@@ -61,6 +94,13 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 		Log = new File("Log.png");
 		Turt = new File("turt.png");
 		FrogIMG = new File("placeholder.jpg");
+		
+		Truck = new File("truck.png");
+		car = new File("car.png");
+		carpng = new File("car2.png");
+		tractor = new File("tractor.png");
+		racecar = new File("racecar.png");
+		
 		
 		frog = new Frog(400, 857, 50, 50, FrogIMG);
 		
@@ -87,6 +127,31 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 		down = new Turtle(900, 450,1);
 		down2 = new Turtle(625, 450,1);
 		down3 = new Turtle(300, 450,1);
+		
+		car1 = new Car(800, 825, 1, true);
+		car2 = new Car(500, 825, 1, true);
+		car3 = new Car(250, 825, 1, true);
+		car4 = new Car(0, 825, 1, true);
+		
+		tractor1 = new Car(800, 760, 2, false);
+		tractor2 = new Car(500, 760, 2, false);
+		tractor3 = new Car(250, 760, 2, false);
+		tractor4 = new Car(0, 760, 2, false);
+		
+		car5 = new Car(800, 700, 3, true);
+		car6 = new Car(500, 700, 3, true);
+		car7 = new Car(250, 700, 3, true);
+		car8 = new Car(0, 700, 3, true);
+		
+		racecar1 = new Car(800, 640, 4, false);
+		racecar2 = new Car(500, 640, 4, false);
+		racecar3 = new Car(250, 640, 4, false);
+		racecar4 = new Car(0, 640, 4, false);
+		
+		truck1 = new Car(800, 580, 5, true);
+		truck2 = new Car(500, 580, 5, true);
+		truck3 = new Car(250, 580, 5, true);
+		truck4 = new Car(0, 580, 5, true);
 	}
 	
 	
@@ -116,6 +181,31 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 			window.drawImage(ImageIO.read(Turt), down.getX(), down.getY(), 200, 75, null);
 			window.drawImage(ImageIO.read(Turt), down2.getX(), down2.getY(), 200, 75, null);
 			window.drawImage(ImageIO.read(Turt), down3.getX(), down3.getY(), 200, 75, null);
+			
+			window.drawImage(ImageIO.read(car), car1.getX(), car1.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(car), car2.getX(), car2.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(car), car3.getX(), car3.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(car), car4.getX(), car4.getY(), 100, 40, null);
+			
+			window.drawImage(ImageIO.read(tractor), tractor1.getX(),  tractor1.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(tractor), tractor2.getX(),  tractor2.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(tractor), tractor3.getX(),  tractor3.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(tractor), tractor4.getX(),  tractor4.getY(), 100, 40, null);
+			
+			window.drawImage(ImageIO.read(carpng), car5.getX(), car5.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(carpng), car6.getX(), car6.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(carpng), car7.getX(), car7.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(carpng), car8.getX(), car8.getY(), 100, 40, null);
+			
+			window.drawImage(ImageIO.read(racecar), racecar1.getX(), racecar1.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(racecar), racecar2.getX(), racecar2.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(racecar), racecar3.getX(), racecar3.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(racecar), racecar4.getX(), racecar4.getY(), 100, 40, null);
+			
+			window.drawImage(ImageIO.read(Truck), truck1.getX(), truck1.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(Truck), truck2.getX(), truck2.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(Truck), truck3.getX(), truck3.getY(), 100, 40, null);
+			window.drawImage(ImageIO.read(Truck), truck4.getX(), truck4.getY(), 100, 40, null);
 			
 			window.drawImage(ImageIO.read(frog.sprite), frog.x, frog.y, frog.w, frog.h, null);
 			
@@ -151,6 +241,33 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 			down.slide();
 			down2.slide();
 			down3.slide();
+			
+			car1.slide();
+			car2.slide();
+			car3.slide();
+			car4.slide();
+			
+			tractor1.slide();
+			tractor2.slide();
+			tractor3.slide();
+			tractor4.slide();
+			
+			car5.slide();
+			car6.slide();
+			car7.slide();
+			car8.slide();
+			
+			racecar1.slide();
+			racecar2.slide();
+			racecar3.slide();
+			racecar4.slide();
+			
+			truck1.slide();
+			truck2.slide();
+			truck3.slide();
+			truck4.slide();
+
+			
 		}
 	}
 	
@@ -176,7 +293,6 @@ public class Screen extends JPanel  implements Runnable, KeyListener  {
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println(keyDown);
 		// TODO Auto-generated method stub
 		if((e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) && keyDown == false) {
 			frog.move(-moveDistance, 0);
